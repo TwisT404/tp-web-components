2d20accc8d6fc6a12a2b415ed3b6bd61b09194da
TwisT404 <cedric.tanghe1303@gmail.com>
2024-11-06 11:12:08 +0100

    Exercice On Template

---
+++ custom-details.js
@@ -0,0 +1,42 @@
+class CustomDetails extends HTMLElement {
+    connectedCallback() {
+        this.attachShadow({ mode: "open"});
+
+        //Récupérer les élements du template
+        const template = document.getElementById("custom-details").content;
+        //Afficher dans la nouvelle balise
+        this.shadowRoot.appendChild(template.cloneNode(true));
+
+
+        const details = this.shadowRoot.querySelector("details");
+
+        
+        details.addEventListener("keydown", (event)=> {
+            if(event.code === "Escape"){
+                details.removeAttribute("open");
+            }
+        });
+
+        details.addEventListener("mouseover", (event)=> {
+            details.toggleAttribute("open");
+            
+        });
+
+        details.addEventListener("focusin", (event)=> {
+            details.toggleAttribute("open");
+            console.log('bojour');
+        });
+
+
+        
+    }
+
+
+    disconnectedCallback() {
+        console.log("Annulé");
+    }
+
+
+
+}
+customElements.define("custom-details", CustomDetails);
